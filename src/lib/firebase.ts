e67144c2f28ff10c5ffc8bceee094045d3b14680
Angel.G <agarzonsarzosa@gmail.com>
2025-07-12 14:43:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { config } from 'dotenv';
+
+config({ path: '.env' });
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
